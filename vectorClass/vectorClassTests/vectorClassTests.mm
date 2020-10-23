1e84088203e961b06b6a8f44fb124168959426b9
theosfa <theos.fa@gmail.com>
2020-10-23 11:43:54 +0300

    Added some tests for vectorClass

--- vectorClass/vectorClassTests/vectorClassTests.mm
+++ vectorClass/vectorClassTests/vectorClassTests.mm
@@ -6,6 +6,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "vectorClass.hpp"
+#import "../vectorClass/vectorClass.cpp"
 
 @interface vectorClassTests : XCTestCase
 
@@ -13,24 +15,207 @@
 
 @implementation vectorClassTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)test_vectorLength_122Inputed_3Returned {
+    // arrange
+    Vector vector_testing(1, 2, 2);
+    double vector_Length;
+    // act
+    vector_Length = vector_testing.vectorLength();
+    // assert
+    XCTAssertEqual(3, vector_Length);
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)test_GettingXYZ_123Inputed_123Returned {
+    // arrange
+    Vector vector_testing(1, 2, 3);
+    double x, y, z;
+    // act
+    x = vector_testing.get_x();
+    y = vector_testing.get_y();
+    z = vector_testing.get_z();
+    // assert
+    XCTAssertEqual(1, x);
+    XCTAssertEqual(2, y);
+    XCTAssertEqual(3, z);
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)test_Cosine_340and442Inputed_14div15Returned {
+    // arrange
+    Vector vector_testing_1(3, 4, 0);
+    Vector vector_testing_2(4, 4, 2);
+    double cosine;
+    // act
+    cosine = vector_testing_1 ^ vector_testing_2;
+    // assert
+    XCTAssertEqual((14.0)/(15.0), cosine);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)test_Sum_122and310Inputed_432Returned {
+    // arrange
+    Vector vector_testing_1(1, 2, 2);
+    Vector vector_testing_2(3, 1, 0);
+    Vector vector_result;
+    double x, y, z;
+    // act
+    vector_result = vector_testing_1 + vector_testing_2;
+    x = vector_result.get_x();
+    y = vector_result.get_y();
+    z = vector_result.get_z();
+    // assert
+    XCTAssertEqual(4, x);
+    XCTAssertEqual(3, y);
+    XCTAssertEqual(2, z);
 }
 
+- (void)test_Minus_122and310Inputed_m212Returned {
+    // arrange
+    Vector vector_testing_1(1, 2, 2);
+    Vector vector_testing_2(3, 1, 0);
+    Vector vector_result;
+    double x, y, z;
+    // act
+    vector_result = vector_testing_1 - vector_testing_2;
+    x = vector_result.get_x();
+    y = vector_result.get_y();
+    z = vector_result.get_z();
+    // assert
+    XCTAssertEqual(-2, x);
+    XCTAssertEqual(1, y);
+    XCTAssertEqual(2, z);
+}
+
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
+//
+//- (void)test_vectorLength_122Inputed_3Returned {
+//    // arrange
+//    Vector vector_testing(1, 2, 2);
+//    // act
+//
+//    // assert
+//    XCTAssertEqual();
+//}
 @end
